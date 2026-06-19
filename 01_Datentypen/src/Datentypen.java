fdb24926b04f2f6c1a789306b8d628a1ae21de84
Kai Schell <kai.schell@oscert.eu>
2026-06-19 13:00:52 +0200

    add datentypen

---
+++ 01_Datentypen/src/Datentypen.java
@@ -0,0 +1,32 @@
+public class Datentypen {
+    static void main(String[] args) {
+        // int: Datentyp (Integer)
+        // ganzeZahl: Name der Variable
+        // 4: Wert der Variable
+        int ganzeZahl = 4;
+        System.out.println(ganzeZahl);
+
+        // 3.14
+        // Mehrere Datentypen für Gleitkommazahlen:
+        // float   -> 32 Bit
+        // double  -> 64 Bit
+        float kommaZahlFloat = 3.14f;
+        double kommaZahlDouble = 3.14;
+        System.out.println(kommaZahlFloat);
+        System.out.println(kommaZahlDouble);
+
+        // Wahrheitswert / Boolean: true oder false
+        boolean wahrheitswert = true;
+        System.out.println(wahrheitswert);
+
+        // einzelnes Zeichen
+        // muss in einfache Hochkommata (') eingefasst werden
+        char zeichen = 'c';
+        System.out.println(zeichen);
+
+        // Zeichenkette / String
+        // muss in doppelte Anführungszeichen (") eingefasst werden
+        String zeichenkette = "huhu";
+        System.out.println(zeichenkette);
+    }
+}

c67334b3f880f1b6eb2d83db1cf599ba23f0c8ef
Kai Schell <kai.schell@oscert.eu>
2026-06-24 11:51:42 +0200

    add schleifen

---
+++ _Uebungen_Besprechung/src/Schleifen.java
@@ -0,0 +1,12 @@
+public class Schleifen {
+    static void main(String[] args) {
+        System.out.println("Übung 3");
+        System.out.println();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}

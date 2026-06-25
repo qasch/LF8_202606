c1c79ef5b6252ea1817c90a43f1ead788ff6012f
Kai Schell <kai.schell@oscert.eu>
2026-06-25 15:30:50 +0200

    add constructor(s)

--- 07_OOP/src/Eissorte.java
+++ 07_OOP/src/Eissorte.java
@@ -6,8 +6,36 @@ public class Eissorte {
     int preis;
     boolean istVerfuegbar;
 
+    // Konstruktor
+    // Standardkonstruktor - ist so in jeder Klasse enthalten
+    // solange wir keinen eigenen Konstruktor definieren
+    //Eissorte() {
+    //
+    //}
+
+    Eissorte() {
+        this.sorte = "Standard";
+        this.preis = 100;
+        this.istVerfuegbar = true;
+    }
+
+    // Konstruktor wird überladen
+    // muss sich in der Parameterliste unterscheiden
+    Eissorte(String sorte) {
+        this.sorte = sorte;
+        this.preis = 100;
+        this.istVerfuegbar = true;
+    }
+
+    Eissorte(String sorte, int preis, boolean istVerfuegbar) {
+        this.sorte = sorte;
+        this.preis = preis;
+        this.istVerfuegbar = istVerfuegbar;
+    }
+
     // Methoden - Funktionalitäten
     void gibNameUndPreisAus() {
-        System.out.println("Ich bin die Sorte " + sorte + " und koste " + preis);
+        System.out.println("Ich bin die Sorte " + this.sorte + ", koste " +
+                this.preis + " und bin verfügbar: " + this.istVerfuegbar);
     }
 }
--- 07_OOP/src/Main.java
+++ 07_OOP/src/Main.java
@@ -1,38 +1,16 @@
 public class Main {
     static void main(String[] args) {
         // Objekt der Klasse Eissorte erzeugen
-        Eissorte vanille = new Eissorte();
-
-        //System.out.println(vanille);
-
-        System.out.println("Objektzustand VOR der Zuweisung konkreter Werte für die Attribute:");
-        System.out.println(vanille.sorte);
-        System.out.println(vanille.preis);
-        System.out.println(vanille.istVerfuegbar);
-
-        vanille.sorte = "Vanille";
-        vanille.preis = 100;
-        vanille.istVerfuegbar = true;
-
-        System.out.println("Objektzustand NACH der Zuweisung konkreter Werte für die Attribute:");
-        System.out.println(vanille.sorte);
-        System.out.println(vanille.preis);
-        System.out.println(vanille.istVerfuegbar);
-
-        System.out.println(vanille.sorte + " - " + vanille.preis);
-
-        Eissorte schoko = new Eissorte();
-        schoko.sorte = "Schoko";
-        schoko.preis = 150;
-        schoko.istVerfuegbar = true;
-
-        System.out.println(schoko.sorte);
-        System.out.println(schoko.preis);
-        System.out.println(schoko.istVerfuegbar);
-
-        System.out.println(schoko.sorte + " - " + schoko.preis);
-        
+        Eissorte vanille = new Eissorte("Vanille", 100, true);
         vanille.gibNameUndPreisAus();
+
+        Eissorte schoko = new Eissorte("Schoko", 150, true);
         schoko.gibNameUndPreisAus();
+
+        Eissorte standard = new Eissorte();
+        standard.gibNameUndPreisAus();
+
+        Eissorte erdbeer = new Eissorte("Erdbeer");
+        erdbeer.gibNameUndPreisAus();
     }
 }
